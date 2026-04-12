cd9896e6caa7d9f059b42f9a6ac22b82e3c60561
rhettelliot <rhettelliot@gmail.com>
2026-04-11 19:50:47 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['172.15.0.73', 'localhost'],
+  allowedDevOrigins: ['172.15.0.73', '172.15.0.48', 'localhost'],
 };
 
 export default nextConfig;
