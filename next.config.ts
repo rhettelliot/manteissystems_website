996cbb12c3ea51d4b60a7335301fdf60a5d157ba
rhettelliot <rhettelliot@gmail.com>
2026-04-23 12:06:10 -0700

    Configure GitHub Pages deployment and custom domain Manteis.Systems

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: ['172.15.0.73', '172.15.0.48', 'localhost'],
 };
 
